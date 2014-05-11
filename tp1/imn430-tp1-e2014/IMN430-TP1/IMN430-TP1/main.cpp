11ad9984a5131599e5f1a50c002a1a19544685de
stevenaubertin <stevenaubertin@gmail.com>
2014-05-11 15:31:56 -0400

    Initial Commit

---
+++ tp1/imn430-tp1-e2014/IMN430-TP1/IMN430-TP1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  IMN430-TP1
+//
+//  Created by Steven Aubertin on 2014-05-11.
+//  Copyright (c) 2014 VISU_TEAM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
